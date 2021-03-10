b4068e92c472a4cb930ac713b500ca8b473cfb9e
Sean Gaudette <seangg1999@gmail.com>
2021-03-09 16:43:42 -0800

    logic commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import './App.css';
-import CalculatorFormSga12 from "./Components/CalculatorFormSga12";
+import CalculatorFormSga12 from "./Components/CalculatorFormSga12/CalculatorFormSga12";
+
+
 
 function App() {
+
+  const [equation, setEquation] = useState({});
+
+  function updateEquation(e) {
+    setEquation({
+    ...equation, 
+
+    [e.target.id] : [e.target.value]
+  })
+}
+
+function doOperation() {
+  switch(equation.operation) {
+    case "add":
+      console.log(parseFloat( equation.operand1) + parseFloat( equation.operand2));
+
+      break;
+
+      default:
+        console.log("sorry")
+        break;
+  }
+}
+
   return (
     <div className="App">
      <h1>This is a calculator</h1>
-     <CalculatorFormSga12></CalculatorFormSga12>
+     <CalculatorFormSga12 />
+     
     </div>
   );
 }
